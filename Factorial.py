960c6a4131e2209c4e27bc91bb5fc05c1aa67b91
genri <h.soome@mail.com>
2019-09-23 22:14:50 +0300

    'fix'

--- Factorial.py
+++ Factorial.py
@@ -3,6 +3,7 @@ sum = 0
 fact = 1
 for i in range(1, n + 1):
     fact = i * fact
+    fact = i * fact
     sum = sum + fact
 
 print(sum)
--- MKAD.py
+++ MKAD.py
@@ -2,5 +2,8 @@ v = int(input())
 t = int(input())
 s = v*t
 alfa = s // 109
+p = s % 109
 s = s - alfa*109
+
 print(s)
+print(p)
